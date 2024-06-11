53261dc86306e061d014ccd535e99c2a2f5e7c1e
Serge Helfrich <serge.helfrich@gmail.com>
2024-06-11 18:00:37 +0200

    Room: exportSchema = false

--- app/src/main/java/org/nitri/opentopo/overlay/OverlayDatabase.kt
+++ app/src/main/java/org/nitri/opentopo/overlay/OverlayDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 import org.nitri.opentopo.overlay.da.MarkerDao
 import org.nitri.opentopo.overlay.model.MarkerModel
 
-@Database(entities = [MarkerModel::class], version = 2)
+@Database(entities = [MarkerModel::class], version = 2, exportSchema = false)
 abstract class OverlayDatabase : RoomDatabase() {
     abstract fun markerDao(): MarkerDao
 
