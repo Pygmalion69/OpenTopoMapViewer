45c8fff10edde13a876983ee57202005655fdb51
Serge Helfrich <serge.helfrich@gmail.com>
2025-10-23 08:39:27 -0700

    Add antipodal point test for distance calculator

--- app/src/test/java/org/nitri/opentopo/DistanceCalculatorTest.kt
+++ app/src/test/java/org/nitri/opentopo/DistanceCalculatorTest.kt
@@ -1,5 +1,6 @@
 package org.nitri.opentopo
 
+import io.ticofab.androidgpxparser.parser.domain.Point
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.nitri.opentopo.util.DistanceCalculator
@@ -29,5 +30,56 @@ class DistanceCalculatorTest {
         val toleranceMeters = 1_000.0
 
         assertEquals(expectedDistanceMeters, distance, toleranceMeters)
+    }
+
+    @Test
+    fun distanceBetweenPointsHandlesAntipodalLocations() {
+        val pointNearGreenwich = createPoint(0.0, 0.0)
+        val antipodalPoint = createPoint(0.0, 180.0)
+
+        val distance = DistanceCalculator.distance(pointNearGreenwich, antipodalPoint)
+
+        val expectedDistanceMeters = Math.PI * 6_371_000.0
+        val toleranceMeters = 10_000.0
+
+        assertEquals(expectedDistanceMeters, distance, toleranceMeters)
+    }
+
+    private fun createPoint(latitude: Double, longitude: Double): Point {
+        val constructor = Point::class.java.declaredConstructors.firstOrNull { ctor ->
+            val parameterTypes = ctor.parameterTypes
+            parameterTypes.size >= 2 &&
+                    parameterTypes[0].isLatitudeLongitudeParameter() &&
+                    parameterTypes[1].isLatitudeLongitudeParameter()
+        } ?: error("Unable to locate Point constructor with latitude/longitude parameters")
+
+        constructor.isAccessible = true
+
+        val parameterTypes = constructor.parameterTypes
+        val arguments = Array<Any?>(parameterTypes.size) { index ->
+            when (index) {
+                0 -> latitude
+                1 -> longitude
+                else -> parameterTypes[index].defaultValue()
+            }
+        }
+
+        return constructor.newInstance(*arguments) as Point
+    }
+
+    private fun Class<*>.isLatitudeLongitudeParameter(): Boolean {
+        return this == java.lang.Double.TYPE || this == java.lang.Double::class.java
+    }
+
+    private fun Class<*>.defaultValue(): Any? = when (this) {
+        java.lang.Double.TYPE -> 0.0
+        java.lang.Float.TYPE -> 0f
+        java.lang.Long.TYPE -> 0L
+        java.lang.Integer.TYPE -> 0
+        java.lang.Boolean.TYPE -> false
+        java.lang.Short.TYPE -> 0.toShort()
+        java.lang.Byte.TYPE -> 0.toByte()
+        java.lang.Character.TYPE -> 0.toChar()
+        else -> null
     }
 }
