f39b0c7a9ac3621f2496cfdfd63e173c54963e69
Pygmalion69 <pygmalion@xs4all.nl>
2026-06-06 13:29:13 +0200

    Prepare Jetpack Compose infrastructure

---
+++ app/src/main/java/org/nitri/opentopo/ui/components/Greeting.kt
@@ -0,0 +1,23 @@
+package org.nitri.opentopo.ui.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.nitri.opentopo.ui.theme.OpenTopoTheme
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    OpenTopoTheme {
+        Greeting("OpenTopo")
+    }
+}
