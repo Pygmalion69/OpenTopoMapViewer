140c7096eccf20cc656b305fa0196bda271ff2bf
Pygmalion69 <pygmalion@xs4all.nl>
2026-06-07 11:45:52 +0200

    Match original app colors

--- app/src/main/java/org/nitri/opentopo/ui/theme/Color.kt
+++ app/src/main/java/org/nitri/opentopo/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package org.nitri.opentopo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary = Color(0xFF009688)
+val PrimaryDark = Color(0xFF00796B)
+val PrimaryLight = Color(0xFFB2DFDB)
+val Accent = Color(0xFF795548)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PrimaryText = Color(0xFF212121)
+val SecondaryText = Color(0xFF757575)
+val ColorIcons = Color(0xFFFFFFFF)
+val Divider = Color(0xFFBDBDBD)
+val Track = Color(0xFFF44336)
--- app/src/main/java/org/nitri/opentopo/ui/theme/Theme.kt
+++ app/src/main/java/org/nitri/opentopo/ui/theme/Theme.kt
@@ -16,21 +16,23 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
+    primary = Primary,
+    secondary = Accent,
+    tertiary = PrimaryLight,
+    onPrimary = ColorIcons,
+    onSecondary = ColorIcons,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Primary,
+    secondary = Accent,
+    tertiary = PrimaryLight,
+    onPrimary = ColorIcons,
+    onSecondary = ColorIcons,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
@@ -58,7 +60,7 @@ fun OpenTopoTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
