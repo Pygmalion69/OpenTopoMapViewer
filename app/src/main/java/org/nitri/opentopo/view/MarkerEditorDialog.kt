cb1d403476db0bd2ca8b865c5ebdb6a7ff13a59e
Pygmalion69 <pygmalion@xs4all.nl>
2026-08-22 09:21:00 +0200

    Add marker editor preview

--- app/src/main/java/org/nitri/opentopo/view/MarkerEditorDialog.kt
+++ app/src/main/java/org/nitri/opentopo/view/MarkerEditorDialog.kt
@@ -36,6 +36,7 @@ import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.setFragmentResult
@@ -298,5 +299,20 @@ private fun MarkerEditorContent(
                 )
             }
         }
+    }
+}
+
+@Preview(showBackground = true, name = "Marker Editor")
+@Composable
+private fun MarkerEditorContentPreview() {
+    OpenTopoTheme(dynamicColor = false) {
+        MarkerEditorContent(
+            initialName = "Summit",
+            initialDescription = "Beautiful view from here",
+            initialColor = DEFAULT_MARKER_COLOR,
+            onNameChange = {},
+            onDescriptionChange = {},
+            onColorChange = {}
+        )
     }
 }
